51ce796f3064e0565b45240d16746424a07923b8
poupou14 <poulnais@gmail.com>
2018-07-24 10:39:25 +0200

    Httplib issue solved by decorator

--- src/GridRequestor.py
+++ src/GridRequestor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import os,string, sys
+import os,string, sys, httplib
 from PySide.QtCore import  QUrl, QThread, Signal, QObject
 from contextlib import closing
 from selenium.webdriver import Firefox # pip install selenium
@@ -20,6 +20,19 @@ class GridRequestor(QThread):
         def setUrl(self, url):
                 self.__url = url
 
+        def pass_bad_status_line_exc(wrapped_function):
+            """
+            Silently pass this exception `http.client.BadStatusLine` decorator
+            """
+            def _wrapper(*args, **kwargs):
+                try:
+                    result = wrapped_function(*args, **kwargs)
+                except httplib.BadStatusLine:
+                    return
+                return result
+            return _wrapper
+
+        @pass_bad_status_line_exc
         def run(self):
                 self.__display = Display(visible=0, size=(800, 600))
                 self.__display.start()
@@ -34,10 +47,12 @@ class GridRequestor(QThread):
                         #lambda x: x.find_element_by_id('someId_that_must_be_on_new_page'))
                         # store it to string variable
                         page_source = browser.page_source
+                        self.distribPageGenerated.sig.emit(page_source)
+                        self.__display.stop()
                         browser.quit()
-                except AttributeError :
+                        
+                except AttributeError:
                     print "browser quit AttributeError issue... don't care, go on !"
+                    pass
                 #print(page_source)
-                self.distribPageGenerated.sig.emit(page_source)
-                self.__display.stop()
                 #self.__display.delete()
